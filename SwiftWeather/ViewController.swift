8534c4657167eaab206233ffe5b98cdb845979db
Jacoblee <jacobleeloveru@gmail.com>
2017-08-24 18:27:58 +0800

    add

--- SwiftWeather/ViewController.swift
+++ SwiftWeather/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    let locationManager:CLLocationManager =
+        CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
